9980747c7cfa2744b293b968a73d3df968746d6b
lephat0816 <lephat.16.08.96@gmail.com>
2025-10-15 08:00:14 +0900

    Initial commit

--- tasks/src/main/java/com/example/tasks/exception/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.tasks.exception;
-
-public class UserNotFoundException extends RuntimeException{
-    public UserNotFoundException(Long id) {
-        super("Cound not found user with" + id);
-    }
-}
